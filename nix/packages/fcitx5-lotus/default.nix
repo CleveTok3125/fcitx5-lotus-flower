5703d4fbd5e8449fe73d26198900671e97e2073e
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-05-14 19:46:22 +0700

    style(nix): format

--- nix/packages/fcitx5-lotus/default.nix
+++ nix/packages/fcitx5-lotus/default.nix
@@ -42,12 +42,13 @@ stdenv.mkDerivation rec {
     fcitx5
     libinput
     libx11
-    (python3.withPackages (ps:
-      with ps; [
+    (python3.withPackages (
+      ps: with ps; [
         pyqt6
         dbus-python
         qtpy
-      ]))
+      ]
+    ))
     qt6.qtbase
     udev
   ];
