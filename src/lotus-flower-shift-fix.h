5c1e2ec1423b27cae23d84be3042b3b58df1a989
CleveTok3125 <CleveTok3125@gmail.com>
2026-06-04 15:04:54 +0700

    feat: auto fix sticky Shift capitalization for words (3+ letters)

---
+++ src/lotus-flower-shift-fix.h
@@ -0,0 +1,12 @@
+#ifndef _FCITX5_LOTUS_FLOWER_SHIFT_FIX_H_
+#define _FCITX5_LOTUS_FLOWER_SHIFT_FIX_H_
+
+#include <string>
+
+namespace lotus_shift_fix {
+
+    std::string fixShift(const std::string& word);
+
+} // namespace lotus_shift_fix
+
+#endif // _FCITX5_LOTUS_FLOWER_SHIFT_FIX_H_
